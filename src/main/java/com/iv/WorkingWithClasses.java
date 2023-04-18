f338bef201f99b86212b9ef19def233d796fdaec
isaiah vang <vang.isaiah3@gmail.com>
2023-04-18 15:15:53 -0400

    second commit, cookie cutter anaolgy

--- src/main/java/com/iv/WorkingWithClasses.java
+++ src/main/java/com/iv/WorkingWithClasses.java
@@ -1,7 +1,34 @@
 package com.iv;
 
+import java.time.LocalDate;
+
+//COOKIE CUTTER ANALOGY//
 public class WorkingWithClasses {
     public static void main(String[] args){
+        Person isaiah =new Person();
+        isaiah.name = "Lil Zay Dawg";
+        isaiah.age = 21;
+        isaiah.birthday = "08-31-2001";
+        isaiah.hasTattoo = false;
 
+        System.out.println(isaiah.name);
+
+//        Person braynel =new Person();
+//        braynel.name = "Braynel";
     }
+}
+
+class Person {
+    // Properties
+    String name;
+    int age;
+    String birthday;
+    Boolean hasTattoo;
+
+
+    // Constructor
+
+    // Methods
+
+
 }
